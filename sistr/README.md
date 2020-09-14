3da0fa658c92e9f3c074d06d6170216ee9fef410
BonnieCSE <bbhuang@ucsd.edu>
2020-09-14 16:31:18 -0700

    update README

--- sistr/README.md
+++ sistr/README.md
@@ -1,33 +1,53 @@
-# Usage of script to run SISTR
+# Running SISTR
 
-## Note: Before running SISTR, lookup tables must be generated first. 
-Custom lookup tables can be created by the user (see `simulations/`) or the user may use the available lookup tables found here (used in Mitra, et. al. 2020): https://drive.google.com/drive/folders/1g70y6z6sU5DVpF6ZGzosNDVFFoXxnqmn?usp=sharing
+## Note: Before running SISTR, lookup tables must first be generated. 
+Custom lookup tables can be created by the user (see `simulations/` for further details) or the user may use the available lookup tables found here (used in Mitra, et. al. 2020): https://drive.google.com/drive/folders/1g70y6z6sU5DVpF6ZGzosNDVFFoXxnqmn?usp=sharing
 
-## Command to run SISTR
-python SISTR_v1.py constant_het denom_het constant_common denom_common eps_bins inFile use_het use_common use_bins num_bins abc_model lrt_model LRT_num_sims 
+## SISTR Usage
+Example command:
+```
+python SISTR_v1.py \
+     --inFile allele_freqs_test.txt \
+     --outFile ssc_test_results.txt 
+```
 
-### Command line arguments explanations:
-constant_het: constant in numerator for heterozygosity error tolerance formula  
-denom_het: constant in denominator for heterozygosity error tolerance formula  
-constant_common: constant in numerator for number of common alleles error tolerance formula  
-denom_common: constant in denominator for number of common alleles error tolerance formula  
-eps_bins: error tolerance for bins summary statistic  
-inFile: file with allele frequencies (see below for format specifications)  
-use_het: whether to use heterozygosity as summary statistic (y or n)  
-use_common: whether to use number of common alleles as summary statistic (y or n)  
-use_bins: whether to use bins as summary statistic (y or n)  
-num_bins: number of bins to use  
-abc_model: folder name for ABC lookup tables  
-lrt_model: folder name for LRT lookup tables  
-LRT_num_sims: number of simulations used for LRT in preprocessing step  
+Required parameters:  
+*`--inFile` name of input file containing allele frequency data (see below for format specifications)  
+*`--outFile` name of output file 
 
-### Example command (with parameters used in Mitra, et. al. 2020):
-python SISTR_v1.py 0.005 3 1 4 0.3 allele_freqs_example.txt y n y 5 eurodem_prior2 eurodem_0810 2000 
+Optional parameters (default values are those used in Mitra, et. al. 2020):
+*`--eps-het-numerator` constant in numerator for heterozygosity error tolerance formula (default value: 0.005) 
+*`--eps-het-denominator` constant in denominator for heterozygosity error tolerance formula (default value: 3)
+*`--eps_bins` error tolerance for bins summary statistic (default value: 0.3)
+*`--num_bins` number of bins to use (default value: 5)
+*`--lrt_num_sims`: number of simulations used to generate LRT lookup tables (default value: 2000) 
+*`--abc-lookup-folder` path to folder containing ABC lookup tables (default path: './../lookup_tables/abc/')
+*`--lrt-lookup-folder` path to folder containing LRT lookup tables (default path: './../lookup_tables/lrt/')
 
-### Input file specifications:
-The input file should be a tab delimited file with 14 columns: chrom, start, end, allele_freqs, total, period, motif, coding, intron, UTR5, UTR3, promoter5kb, intergenic, tss_gene.  
-Each row should be a separate locus. See example input file 'allele_freqs_example.txt'.
+## Input file format:
+The input file should be a tab delimited file with 7 columns: chrom, start, end, allele_freqs, total, period, motif  
+Each row should be a separate locus.  
+See example input file 'allele_freqs_example.txt'.
 
-### Output file:
-The output file will be a tab delimited file with 24 columns: chrom, start, end, total, period, optimal_ru, motif, coding, intron, UTR5, UTR3, promoter5kb, intergenic, tss_gene, het, common, bins, ABC_s_95_CI, Num_s_accepted_(max_10000), Likelihood_0, Likelihood_s, LR, LogLR, LRT_p_value.  
-Each row is a separate locus.
+### Explanation of each column:
+*chrom: chromosome number
+*start: starting position of locus
+*end: ending position of locus
+*allele_freqs: allele frequency data 
+*total: sample size (number of alleles) of empirical allele frequencies
+*period: number of base pairs in the repeat motif (e.g. period 2 refers to dinucleotides)
+*motif: repeat motif
+
+Note: Fomrat of allele frequency data
+Data for each allele present in the population is represented by two numbers separated by a colon. The first number is the allele size in base pairs relative to the reference allele size (which is calculated as end - start + 1). The second number is the number of copies of the allele in the population. Each allele is separated by a comma. 
+   For example: 
+   | chrom | start | end | allele_freqs | total | period | motif |
+   |---|---|---|---|---|---|---|
+   |1|1000|1025|-2:100,0:500,4:200|6500|2|AT| 
+   At this locus, the reference allele length is 26 base pairs or 13 repeat units. In the population, there are 3 alleles present: 100 copies of 12 repeat units, 500 copies of 13 repeat units, and 200 copies of 15 repeat units.
+
+
+## Output file format:
+The output file will be a tab delimited file with 14 columns: chrom, start, end, total, period, optimal_ru, motif, ABC_s_95_CI, Percent_s_accepted, Likelihood_0, Likelihood_s, LR, LogLR, LRT_p_value    
+Each row is a separate locus.  
+See example output file 'test_results.txt'  
--- sistr/SISTR_v1.py
+++ sistr/SISTR_v1.py
@@ -1,68 +1,47 @@
 # Script to run SISTR: 
-# Method to obtain posterior estimate of s and corresponding p value for each STR
+# A method to obtain a posterior estimate of s and corresponding p value for each STR
 
 # Imports 
-import matplotlib
-matplotlib.use('Agg')
-matplotlib.rcParams['pdf.fonttype'] = 42
-matplotlib.rcParams['ps.fonttype'] = 42
-from matplotlib import pyplot as plt
 from scipy.stats import geom
 import copy
-import sys
-sys.path.append("/storage/BonnieH/selection_project/helper_functions")
+import argparse
 from LRT_functions import *
 
 ### Main function ###
 def main():
     
     # Load parameters
-    constant_het = float(sys.argv[1])
-    denom_het = int(sys.argv[2])
-    constant_common = int(sys.argv[3])
-    denom_common = int(sys.argv[4])
-    eps_bins = float(sys.argv[5])
-    inFile = sys.argv[6]
-    use_het = sys.argv[7]
-    use_common = sys.argv[8]
-    use_bins = sys.argv[9]
-    num_bins = int(sys.argv[10])
-    abc_model = sys.argv[11]
-    lrt_model = sys.argv[12]
-    LRT_num_sims = int(sys.argv[13])
+    parser = argparse.ArgumentParser()
     
-    # Name output files
-    file_type = ''
-    if inFile == '/storage/BonnieH/selection_project/ssc_files/0810/allele_freqs_filt_test.txt':
-        file_type = '_test_0810'
-    if inFile == '/storage/BonnieH/selection_project/ssc_files/0810/allele_freqs_filt.txt':
-        file_type = '_all_per_0810'
+    parser.add_argument("--eps-het-numerator", type=float, default=0.005)
+    parser.add_argument("--eps-het-denominator", type=int, default=3)
+    parser.add_argument("--eps-bins", type=float, default=0.3)
+    parser.add_argument("--num-bins", type=int, default=5)
+    parser.add_argument("--lrt-num-sims", type=int, default=2000)
+    parser.add_argument("--abc-lookup-folder", default = './../lookup_tables/abc/')
+    parser.add_argument("--lrt-lookup-folder", default = './../lookup_tables/lrt/')
+    parser.add_argument("--inFile")
+    parser.add_argument("--outFile")
+    
+    args = parser.parse_args()
+    
+    constant_common = 1
+    denom_common = 1
+    
+    use_het = 'y'
+    use_common = 'n'
+    use_bins = 'y'
         
-    filename = str(constant_het) + "_" + str(denom_het) + "_" + str(eps_bins) + \
-               "_" + use_het + use_common + use_bins + str(num_bins) + "_" + abc_model + file_type
-    
-    outFile = '/storage/BonnieH/selection_project/per_locus/SISTR_results_0825/' + filename + '.txt'
-    figFile = '/storage/BonnieH/selection_project/per_locus/SISTR_results_0825/figs/' + filename + '.png'
-    statsFile = '/storage/BonnieH/selection_project/per_locus/SISTR_results_0825/stats/' + filename + '.txt'
-    
-    # Open output files
-    allele_freqs_file = open(inFile, 'r')
-    stats_file = open(statsFile, 'w')
-    results = open(outFile, "w")
+    # Open input and output files
+    allele_freqs_file = open(args.inFile, 'r')
+    header = allele_freqs_file.readline().strip().split('\t')
+    results = open(args.outFile, "w")
 
     # Write results header
     results.write("chrom" + "\t" + "start" + "\t" + "end" + "\t" + "total" + "\t" + "period" + \
-                  "\t" + "optimal_ru" + "\t" + "motif" + "\t" + "coding" + "\t"  + 'intron' + \
-                  '\t' + 'UTR5' + '\t' + 'UTR3' + '\t' + 'promoter5kb' + '\t' + 'intergenic' + \
-                  "\t" + "tss_gene" + "\t" + "het" + "\t" + "common" + "\t" + "bins" + "\t" + \
-                  "ABC_s_median" + "\t" + "ABC_s_95%_CI" + "\t" + "Num_s_accepted_(max_10000)" + "\t" + \
-                  "Likelihood_0" + "\t" + "Likelihood_s" + "\t" + "LR" + "\t" + "LogLR" + \
-                  "\t" + "LRT_p_value" + "\n")
-
-    total_lines_acc = 0 # Total lines with >10 s accepted
-    total_lines = 0 
-    lower_0 = 0 # Total lines where lower bound is 0
-    s_acc = [] # List of percent of s accepted
+                  "\t" + "optimal_ru" + "\t" + "motif" + "\t" + "ABC_s_median" + "\t" + "ABC_s_95%_CI" + \
+                  "\t" + "Percent_s_accepted" + "\t" + "Likelihood_0" + "\t" + "Likelihood_s" + "\t" + \
+                  "LR" + "\t" + "LogLR" + "\t" + "LRT_p_value" + "\n")
      
     # Preprocess ABC lookup table
     # Get ABC tables
@@ -78,8 +57,8 @@ def main():
         for per_opt in opt_allele_dic_w_per[per]:
             period = per_opt[0]
             opt_allele = per_opt[1]
-            file = '/gymreklab-tscc/bonnieh/abc/results/' + abc_model + '/' + str(period) + '_' + str(opt_allele) + '.txt' 
-            table = GetABCList(file, num_bins)
+            file = args.abc_lookup_folder + str(period) + '_' + str(opt_allele) + '.txt' 
+            table = GetABCList(file, args.num_bins)
 
             ABC_tables[per_opt] = table
         
@@ -87,7 +66,6 @@ def main():
     for line in allele_freqs_file:
         
         # Get information from line
-        total_lines = total_lines + 1
         info = line.strip().split('\t')
         chrom = info[0]
         start = int(info[1])
@@ -96,33 +74,23 @@ def main():
         total = info[4]
         per = int(info[5])
         motif = info[6]
-        coding = info[7]
-        intron = info[8]
-        UTR5 = info[9]
-        UTR3 = info[10]
-        promoter5kb = info[11]
-        intergenic = info[12]
-        gene = info[13]
 
         # Get optimal allele and allele_freqs
         opt_allele, allele_freqs = Process_Freqs(freqs, per, end, start)
         freq_string = ','.join(str(round(item, 5)) for item in allele_freqs)
         
         # Add 0s to allele frequency list if number of alleles less than number of bins
-        if len(allele_freqs) < num_bins:
-            num_zeros_to_add = int((num_bins - len(allele_freqs))/2)
+        if len(allele_freqs) < args.num_bins:
+            num_zeros_to_add = int((args.num_bins - len(allele_freqs))/2)
             for i in range(0, num_zeros_to_add):
                 freq_string = '0.0,' + freq_string
                 freq_string = freq_string + ',0.0'
                 
         # Get summary stats
-        obs_het, obs_common, obs_bins = GetSummStats(freq_string, num_bins)
+        obs_het, obs_common, obs_bins = GetSummStats(freq_string, args.num_bins)
         
         results.write(chrom + '\t' + str(start) + '\t' + str(end) + '\t' + total + '\t' + str(per) + \
-                      '\t' + str(opt_allele) + '\t' + motif + '\t' + coding + '\t'  + intron + \
-                      '\t' + UTR5 + '\t' + UTR3 + '\t' + promoter5kb + '\t' + intergenic + \
-                      '\t' + gene + '\t' + str(round(obs_het, 7)) + '\t' + str(obs_common) + '\t' + \
-                      ','.join(str(round(item,4)) for item in obs_bins) + '\t')
+                      '\t' + str(opt_allele) + '\t' + motif + '\t')
         
         # All optimal alleles > 13 have the same mutation rate as optimal allele 13
         if per == 3 and opt_allele > 13: 
@@ -145,22 +113,22 @@ def main():
         
         # Perform ABC
         s_ABC, lower_bound, upper_bound, num_accepted, s_accepted = Get_S_ABC(abc_list, 
-                                       obs_het, obs_common, obs_bins, constant_het, 
-                                       denom_het, constant_common, denom_common, eps_bins, use_het, 
+                                       obs_het, obs_common, obs_bins, args.eps_het_numerator, 
+                                       args.eps_het_denominator, constant_common, denom_common, args.eps_bins, use_het, 
                                        use_common, use_bins)
         
         # Write N/A if <10 s accepted during ABC
         if s_ABC == -1:
             s_ABC = 'N/A'
             ABC_conf_int = 'N/A'
-            num_accepted = '<10'
+            num_accepted = 'N/A'
             likelihood_0 = 'N/A'
             likelihood_s_ABC = 'N/A'
             LR = 'N/A'
             LogLR = 'N/A'
             pval = 'N/A'
         
-            results.write(str(s_ABC) + '\t' + ABC_conf_int + '\t' + str(num_accepted) + '\t')
+            results.write(str(s_ABC) + '\t' + ABC_conf_int + '\t' + num_accepted + '\t')
             results.write(str(likelihood_0) + '\t' + str(likelihood_s_ABC) + '\t' + \
                           str(LR) + '\t' + str(LogLR) + '\t' + str(pval) + '\n')
          
@@ -182,18 +150,9 @@ def main():
             else:
                 upper_bound = round(upper_bound, 5)
 
-            if lower_bound == 0:
-                lower_0 = lower_0 + 1
-
-            if lower_bound != -1:
-                total_lines_acc = total_lines_acc + 1
-
-            if num_accepted >= 10:
-                s_acc.append(num_accepted)
-
             ABC_conf_int = '(' + str(lower_bound) + ' , ' + str(upper_bound) + ')'
 
-            results.write(str(s_ABC) + '\t' + ABC_conf_int + '\t' + str(num_accepted) + '\t')
+            results.write(str(s_ABC) + '\t' + ABC_conf_int + '\t' + str(round(num_accepted/len(ABC_tables[(3,5)])*100,4)) + '\t')
 
             s_ABC_round = get_LRT_bin(s_ABC)
 
@@ -201,8 +160,7 @@ def main():
 
             ### Get list of s values in LRT simulations file ###
             s_list_available = []
-            lrtFile = '/gymreklab-tscc/bonnieh/lrt/results/' + lrt_model + '/' \
-            + str(per) + '_' + str(opt_allele) + '_freqs.txt' 
+            lrtFile = args.lrt_lookup_folder + str(per) + '_' + str(opt_allele) + '_freqs.txt' 
             
             lrt_file = open(lrtFile, 'r')
             header = lrt_file.readline().strip()
@@ -218,7 +176,7 @@ def main():
                 s_ABC_round = getNearestS(s_ABC_round, s_list_available)
 
             # Get LRT summary statistic tables for s = 0
-            lrtFile_for_s_0 = '/gymreklab-tscc/bonnieh/lrt/results/' + lrt_model + '/' + str(per) + '_' + str(opt_allele) + '_15_freqs.txt' 
+            lrtFile_for_s_0 = args.lrt_lookup_folder + str(per) + '_' + str(opt_allele) + '_zero_freqs.txt' 
             freqs_list_raw_0 = GetLRTListByRow(lrtFile_for_s_0, 0)
             LRT_table_0_het = []
             LRT_table_0_common = []
@@ -227,7 +185,7 @@ def main():
             # Get summary statistics from allele frequencies
             for freq_string in freqs_list_raw_0:
 
-                obs_het_0, obs_common_0, obs_bins_0 = GetSummStats(freq_string, num_bins)
+                obs_het_0, obs_common_0, obs_bins_0 = GetSummStats(freq_string, args.num_bins)
                 LRT_table_0_het.append(obs_het_0) 
                 LRT_table_0_common.append(obs_common_0) 
                 LRT_table_0_bins.append(obs_bins_0)
@@ -245,7 +203,7 @@ def main():
             # Get summary statistics from allele frequencies
             for freq_string in freqs_list_raw_s:
 
-                obs_het_s, obs_common_s, obs_bins_s = GetSummStats(freq_string, num_bins)
+                obs_het_s, obs_common_s, obs_bins_s = GetSummStats(freq_string, args.num_bins)
                 LRT_table_s_het.append(obs_het_s) 
                 LRT_table_s_common.append(obs_common_s) 
                 LRT_table_s_bins.append(obs_bins_s)
@@ -253,9 +211,9 @@ def main():
             # Perform LRT
             likelihood_0, likelihood_s_ABC, LR, LogLR, pval = LikelihoodRatioTest(LRT_table_0_het, \
                                     LRT_table_s_het, LRT_table_0_common, LRT_table_s_common, \
-                                    LRT_table_0_bins, LRT_table_s_bins, LRT_num_sims, \
-                                    obs_het, obs_common, obs_bins, constant_het, denom_het, \
-                                    constant_common, denom_common, eps_bins, use_het, use_common, use_bins)
+                                    LRT_table_0_bins, LRT_table_s_bins, args.lrt_num_sims, \
+                                    obs_het, obs_common, obs_bins, args.eps_het_numerator, args.eps_het_denominator, \
+                                    constant_common, denom_common, args.eps_bins, use_het, use_common, use_bins)
 
             results.write(str(round(likelihood_0, 7)) + '\t' + str(round(likelihood_s_ABC, 7)) + '\t' + \
                               str(round(LR, 7)) + '\t' + str(round(LogLR ,7)) + '\t' + str(round(pval, 7)) + '\n')
@@ -263,32 +221,6 @@ def main():
     allele_freqs_file.close()
             
     results.close()
-    
-    # Output statistics
-    stats_file.write("Total lines: " + str(total_lines) + "\n")
-    percent_acc = total_lines_acc/total_lines
-    stats_file.write("Total accepted lines: " + str(total_lines_acc) + " " + str(percent_acc) + "\n")
-    percent_0 = lower_0/total_lines
-    stats_file.write("Lower bound for s is zero: " + str(lower_0) + " " + str(percent_0) + "\n")
-    
-    for i in range(0, len(s_acc)):
-        s_acc[i] = s_acc[i]/10000 * 100
-    
-    s_acc_med = np.median(s_acc)
-    s_acc_mean = np.mean(s_acc)
-
-    stats_file.write("s_acc median: " + str(s_acc_med) + "\n")
-    stats_file.write("s_acc mean: " + str(s_acc_mean) + "\n")
- 
-    plt.figure(1)
-    buckets = list(np.arange(0, 101, 1)) 
-    
-    plt.hist(s_acc, bins=buckets, weights=np.ones(len(s_acc)) / len(s_acc))
-
-    plt.title('Percent s accepted during ABC %s model \n const_het %.5f, denom_het = %d, num_bins = %d \n Summ stats used: het = %s common alleles = %s bins = %s'%(abc_model, constant_het, denom_het, num_bins, use_het, use_common, use_bins))
-    plt.xlabel("% s accepted")
-    plt.ylabel("Frequency")
-    plt.savefig(figFile, bbox_inches='tight')
     
 if __name__ == '__main__':
     main()
